d95da0906dc1f96e40c3bfb1172cc991da7a3f9a
Huynh Van Huu An <huynhvahuuan3620@gmail.com>
2023-01-31 12:56:12 +0700

    test notification

--- lib/screen/setting/setting_screen.dart
+++ lib/screen/setting/setting_screen.dart
@@ -44,107 +44,143 @@ class _SettingScreenState extends State<SettingScreen> {
             SingleChildScrollView(
               child: Column(
                 children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: getProportionateScreenWidth(20),
-                    ),
-                    child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          isClickLogout = false;
-                        });
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title: const Text("Đăng xuất"),
-                              content: const Text("Bạn có muốn đăng xuất không?"),
-                              actions: [
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                    setState(() {
-                                      isClickLogout = false;
-                                    });
-                                  },
-                                  child: const Text("Hủy"),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    provider.logout();
-                                    Navigator.pushAndRemoveUntil(
-                                      context,
-                                      PageRouteBuilder(
-                                        pageBuilder: (context, animation1, animation2) => const LoginScreen(),
-                                        transitionsBuilder: (context, animation1, animation2, child) => SlideTransition(
-                                          position: Tween(
-                                            // slide from left to right
-                                            begin: const Offset(-1, 0),
-                                            end: Offset.zero,
-                                          ).animate(animation1),
-                                          child: child,
-                                        ),
-                                      ),
-                                      (route) => false,
-                                    );
-                                  },
-                                  child: const Text("Đăng xuất"),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      onTapDown: (details) {
-                        setState(() {
-                          isClickLogout = true;
-                        });
-                      },
-                      child: Container(
-                        padding: EdgeInsets.symmetric(
-                          vertical: getProportionateScreenHeight(15),
-                          horizontal: getProportionateScreenWidth(20),
-                        ),
-                        margin: EdgeInsets.only(
-                          bottom: getProportionateScreenHeight(10),
-                        ),
-                        decoration: BoxDecoration(
-                          color: isClickLogout ? primaryColor.withOpacity(0.5) : Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 1,
-                              blurRadius: 7,
-                              offset: const Offset(0, 3), // changes position of shadow
-                            ),
-                          ],
-                        ),
-                        child: Row(
-                          children: [
-                            Text(
-                              "Đăng xuất",
-                              style: TextStyle(
-                                fontSize: getProportionateScreenWidth(18),
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black,
+                  SettingTile(
+                    title: "Đăng xuất",
+                    icon: Icons.logout,
+                    color: primaryColor,
+                    onPress: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text("Đăng xuất"),
+                            content: const Text("Bạn có muốn đăng xuất không?"),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text("Hủy"),
                               ),
-                            ),
-                            const Spacer(),
-                            Icon(
-                              Icons.logout_rounded,
-                              color: Colors.red,
-                              size: getProportionateScreenWidth(20),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                              TextButton(
+                                onPressed: () {
+                                  provider.logout();
+                                  Navigator.pushAndRemoveUntil(
+                                    context,
+                                    PageRouteBuilder(
+                                      pageBuilder: (context, animation1, animation2) => const LoginScreen(),
+                                      transitionsBuilder: (context, animation1, animation2, child) => SlideTransition(
+                                        position: Tween(
+                                          // slide from left to right
+                                          begin: const Offset(-1, 0),
+                                          end: Offset.zero,
+                                        ).animate(animation1),
+                                        child: child,
+                                      ),
+                                    ),
+                                    (route) => false,
+                                  );
+                                },
+                                child: const Text("Đăng xuất"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
                   ),
                 ],
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingTile extends StatefulWidget {
+  const SettingTile({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.color,
+    required this.onPress,
+  });
+
+  final String title;
+  final IconData icon;
+  final Color color;
+  final VoidCallback onPress;
+
+  @override
+  State<SettingTile> createState() => _SettingTileState();
+}
+
+class _SettingTileState extends State<SettingTile> {
+  bool isClick = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: getProportionateScreenWidth(20),
+      ),
+      child: GestureDetector(
+        onTap: widget.onPress,
+        onTapCancel: () {
+          setState(() {
+            isClick = false;
+          });
+        },
+        onTapDown: (details) {
+          setState(() {
+            isClick = true;
+          });
+        },
+        onTapUp: (details) {
+          setState(() {
+            isClick = false;
+          });
+        },
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            vertical: getProportionateScreenHeight(15),
+            horizontal: getProportionateScreenWidth(20),
+          ),
+          margin: EdgeInsets.only(
+            bottom: getProportionateScreenHeight(10),
+          ),
+          decoration: BoxDecoration(
+            color: isClick ? primaryColor.withOpacity(0.5) : Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 7,
+                offset: const Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              Text(
+                "Đăng xuất",
+                style: TextStyle(
+                  fontSize: getProportionateScreenWidth(18),
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              ),
+              const Spacer(),
+              Icon(
+                Icons.logout_rounded,
+                color: Colors.red,
+                size: getProportionateScreenWidth(20),
+              ),
+            ],
+          ),
         ),
       ),
     );
